4c347e9b0f171db4d560e5247979ad5cb8622490
Lanes Rocha <lanes.rocha@admin.uvv>
2020-11-19 21:52:16 -0300

    Criação do Algorítimo SelectionSort

--- aula07 - Ordenacao/Program.cs
+++ aula07 - Ordenacao/Program.cs
@@ -6,9 +6,24 @@ class Program
     static void Main(string[] args)
     {
         int[] vetor = new int[] {1,5,6,45,0,-1,12};
+        int[] vetor2 = new int[] {3,5,9,-45,2,-1,1};
+
         Ordenador order = new Ordenador();
+        Ordenador order2 = new Ordenador();
+        
+        Console.WriteLine();
+        Console.WriteLine("Primeira impressão utilizando Bubblesort");
         order.Bubblesort(vetor);
         order.PrintVetor(vetor);
+
+        Console.WriteLine();
+
+        Console.WriteLine("=======================================");
+        Console.WriteLine();        
+
+        Console.WriteLine("Segunda impressão utilizando SelectionSort");
+        order2.SeletionSort(vetor2);
+        order2.PrintVetor(vetor2);
     }
 }
 
