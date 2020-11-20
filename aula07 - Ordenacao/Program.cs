8166a4a3a3f52dd345ed03a888ee0e13154ab11f
Lanes Rocha <lanes.rocha@admin.uvv>
2020-11-20 20:59:46 -0300

    Criação da método de ordenação Selectionsort

--- aula07 - Ordenacao/Program.cs
+++ aula07 - Ordenacao/Program.cs
@@ -5,25 +5,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] vetor = new int[] {1,5,6,45,0,-1,12};
-        int[] vetor2 = new int[] {3,5,9,-45,2,-1,1};
+        int[] vetor = new int[] {1,5,6,45,0,-1,12}; 
+        Ordenador order = new Ordenador();        
 
-        Ordenador order = new Ordenador();
-        Ordenador order2 = new Ordenador();
-        
-        Console.WriteLine();
-        Console.WriteLine("Primeira impressão utilizando Bubblesort");
-        order.Bubblesort(vetor);
+        order.SeletionSort(vetor);// se for trocar a forma de ordenção troca apenas SeletionSort por Bubblesort 
         order.PrintVetor(vetor);
-
-        Console.WriteLine();
-
-        Console.WriteLine("=======================================");
-        Console.WriteLine();        
-
-        Console.WriteLine("Segunda impressão utilizando SelectionSort");
-        order2.SeletionSort(vetor2);
-        order2.PrintVetor(vetor2);
+       
     }
 }
 
