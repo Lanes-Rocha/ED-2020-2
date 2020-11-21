f7f87c1736571f9ee84da1ec72cc6cf28941a63d
Lanes Rocha <lanes.rocha@admin.uvv>
2020-11-20 22:02:19 -0300

    atualização bubblesort v2

--- aula07 - Ordenacao/Program.cs
+++ aula07 - Ordenacao/Program.cs
@@ -8,7 +8,7 @@ class Program
         int[] vetor = new int[] {1,5,6,45,0,-1,12}; 
         Ordenador order = new Ordenador();        
 
-        order.SeletionSort(vetor);// trocar a forma de ordenção é só trocar apenas SeletionSort por Bubblesort 
+        order.Bubblesortv2(vetor);// trocar a forma de ordenção é só trocar apenas SeletionSort por Bubblesort 
         order.PrintVetor(vetor);
        
     }
