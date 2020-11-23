a318dd3ad86a3ccce646770b2d56a112c986c9c9
Lanes Rocha <lanes.rocha@admin.uvv>
2020-11-23 20:04:24 -0300

    Criação do objeto Random

--- aula07 - Ordenacao/Program.cs
+++ aula07 - Ordenacao/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.Diagnostics;
 
 
 class Program
 {
     static void Main(string[] args)
     {
-        int[] vetor = new int[] {1,5,6,45,0,-1,12}; 
+        Stopwatch sw = new Stopwatch();
+        Random random = new Random(); // Criando objeto random
+
+        //int[] vetor = new int[] {1,5,6,45,0,-1,12}; 
+        int[] vetor = new int[1000];
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = random.Next(); // vetor com preencimento randômico.
+        }
+
         Ordenador order = new Ordenador();        
 
+        sw.Start();
         order.InsertonSort(vetor);// trocar a forma de ordenção é só trocar apenas SeletionSort por Bubblesort ou InsertonSort
-        order.PrintVetor(vetor);
+        sw.Stop();
+
+        //order.PrintVetor(vetor);
+        Console.WriteLine($"Runtime = {sw.Elapsed.ToString()}");
        
     }
 
