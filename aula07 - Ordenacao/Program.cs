ffb311e745a10dad35649db08a10c6d062d92a7e
Lanes Rocha <lanes.rocha@admin.uvv>
2020-11-23 20:52:37 -0300

    Atualizando a class Programa.cs

--- aula07 - Ordenacao/Program.cs
+++ aula07 - Ordenacao/Program.cs
@@ -10,20 +10,43 @@ class Program
         Random random = new Random(); // Criando objeto random
 
         //int[] vetor = new int[] {1,5,6,45,0,-1,12}; 
-        int[] vetor = new int[1000];
-        for (int i = 0; i < vetor.Length; i++)
+        int[] vetor1 = new int[1000];
+        for (int i = 0; i < vetor1.Length; i++)
         {
-            vetor[i] = random.Next(); // vetor com preencimento randômico.
+            vetor1[i] = random.Next(); // vetor com preencimento randômico.
         }
+        int[] vetor2 = new int[1000];
+        vetor1.CopyTo(vetor2, 0);
+        int[] vetor3 = new int[1000];
+        vetor1.CopyTo(vetor3, 0);
+        int[] vetor4 = new int[1000];
+        vetor1.CopyTo(vetor4, 0);
 
         Ordenador order = new Ordenador();        
 
         sw.Start();
-        order.InsertonSort(vetor);// trocar a forma de ordenção é só trocar apenas SeletionSort por Bubblesort ou InsertonSort
+        order.SeletionSort(vetor1);// trocar a forma de ordenção é só trocar apenas SeletionSort por Bubblesort ou InsertonSort
         sw.Stop();
+        Console.WriteLine($"Selection Sort = {sw.Elapsed.ToString()}");
+
+        sw.Start();
+        order.Bubblesort(vetor2);// trocar a forma de ordenção é só trocar apenas SeletionSort por Bubblesort ou InsertonSort
+        sw.Stop();
+        Console.WriteLine($"Bubble Sort    = {sw.Elapsed.ToString()}");
+
+        sw.Start();
+        order.Bubblesortv2(vetor3);// trocar a forma de ordenção é só trocar apenas SeletionSort por Bubblesort ou InsertonSort
+        sw.Stop();
+        Console.WriteLine($"Bubble Sort v2 = {sw.Elapsed.ToString()}");
+
+        sw.Start();
+        order.InsertonSort(vetor4);// trocar a forma de ordenção é só trocar apenas SeletionSort por Bubblesort ou InsertonSort
+        sw.Stop();
+        Console.WriteLine($"InsertonSort   = {sw.Elapsed.ToString()}");
+        
 
         //order.PrintVetor(vetor);
-        Console.WriteLine($"Runtime = {sw.Elapsed.ToString()}");
+       
        
     }
 
