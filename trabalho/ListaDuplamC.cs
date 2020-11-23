ab1da210a8236d5e5674c40e64dd8a39982de6a3
Lanes Rocha <lanes.rocha@hotmail.com>
2020-11-22 23:47:59 -0300

    Atualização

---
+++ trabalho/ListaDuplamC.cs
@@ -0,0 +1,278 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.IO;
+using System.Text;
+
+    class ListaDuplamC
+    {
+        public Node head { get; set; }
+
+        public ListaDuplamC()
+        {
+            head = null;
+        }
+
+        public void Add(Contato contato)
+        {
+            Node newNode = new Node(contato);
+
+            if (head == null)
+            {
+                newNode.prev = newNode.next = newNode;
+                head = newNode;
+
+                return;
+            }
+
+            Node last = head.prev;
+
+            newNode.next = head;
+            newNode.prev = last;
+
+            last.next = head.prev = newNode;
+        }
+
+        public Contato Find(string email)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Lista Vazia");
+
+                return null;
+            }
+
+            Node temp = head;
+            do
+            {
+                if (temp.data.email == email)
+                {
+                    return temp.data;
+                }
+                temp = temp.next;
+            } while (temp != head);
+
+            return null;
+        }
+
+        public bool DeleteNode(string email)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Lista Vazia.");
+                return false;
+            }
+
+            Node curr = head;
+            Node prev = null;
+
+            while (curr.data.email != email)
+            {
+                if (curr.next == head)
+                {
+                    return false;
+                }
+                prev = curr;
+                curr = curr.next;
+            }
+
+            if (curr.next == head && prev == null)
+            {
+                head = null;
+                return true;
+            }
+
+            if (curr == head)
+            {
+                prev = head.prev;
+                head = head.next;
+
+                prev.next = head;
+                head.prev = prev;
+            }
+            else if (curr.next == head)
+            {
+                prev.next = head;
+                head.prev = prev;
+            }
+            else
+            {
+                Node temp = curr.next;
+
+                prev.next = temp;
+                temp.prev = prev;
+            }
+
+            return true;
+        }
+
+        public int Length()
+        {
+            if (head == null)
+            {
+                return 0;
+            }
+
+            int size = 0;
+            Node temp = head;
+            do
+            {
+                size++;
+                temp = temp.next;
+            }
+            while (temp != head);
+
+            return size;
+        }
+
+        public void BubbleSort(string field)
+        {
+            bool swapped;
+            Node temp;
+            int length = Length();
+
+            if (length <= 0)
+            {
+                Console.WriteLine("Lista Vazia.");
+                return;
+            }
+
+            do
+            {
+                swapped = false;
+
+                temp = head;
+                for (int i = 0; i < length - 1; i++)
+                {
+                    string compareFieldA;
+                    string compareFieldB;
+
+                    if (field == "nome")
+                    {
+                        compareFieldA = temp.data.nome;
+                        compareFieldB = temp.next.data.nome;
+                    }
+                    else
+                    {
+                        compareFieldA = temp.data.email;
+                        compareFieldB = temp.next.data.email;
+                    }
+
+                    if (String.Compare(compareFieldA, compareFieldB) > 0)
+                    {
+                        Contato t = temp.data;
+                        temp.data = temp.next.data;
+                        temp.next.data = t;
+                        swapped = true;
+                    }
+                    temp = temp.next;
+                }
+            } while (swapped);
+        }
+
+        public void Print()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Lista Vazia");
+
+                return;
+            }
+
+            Node temp = head;
+            do
+            {
+                Console.WriteLine("Contato:");
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            } while (temp != head);
+        }
+
+        public bool PrintFile(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+                using (FileStream fs = File.Create(path))
+                {
+                    if (head == null)
+                    {
+                        Console.WriteLine("Lista Vazia");
+
+                        return true;
+                    }
+
+                    Node temp = head;
+                    while (temp.next != head)
+                    {
+                        AddText(fs, temp.data.nome + "\n");
+                        AddText(fs, temp.data.telefone + "\n");
+                        AddText(fs, temp.data.email + "\n");
+                        temp = temp.next;
+                    }
+                    AddText(fs, temp.data.nome + "\n");
+                    AddText(fs, temp.data.telefone + "\n");
+                    AddText(fs, temp.data.email);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+
+        public bool LoadFile(string path)
+        {
+            while (head != null)
+            {
+                DeleteNode(head.data.email);
+            }
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    return false;
+                }
+
+                using (StreamReader file = new StreamReader(path))
+                {
+                    string nome;
+                    string telefone;
+                    string email;
+                    while (true)
+                    {
+                        nome = file.ReadLine();
+
+                        telefone = file.ReadLine();
+                        if (telefone == null) break;
+
+                        email = file.ReadLine();
+                        if (email == null) break;
+
+                        Add(new Contato(nome, telefone, email));
+                    }
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+    }
+
---
+++ trabalho/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace trabalho
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            ListaDuplamC list = new ListaDuplamC();
+            int menu;
+            while (true)
+            {
+                
+                Console.WriteLine("1        - Adicionar");
+                Console.WriteLine("2        - Remover");
+                Console.WriteLine("3        - Atualizar");
+                Console.WriteLine("4        - Recuperar");
+                Console.WriteLine("5        - Ordenar");
+                Console.WriteLine("6        - Listar");
+                Console.WriteLine("7        - Salvar");
+                Console.WriteLine("8        - Navegar");
+                Console.WriteLine("0        - Sair");
+                Console.WriteLine("Opcao: ");
+                menu = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                if (menu == 1)
+                {
+                    string nome, telefone, email;
+
+                    Console.WriteLine("Novo Contato:");
+
+                    Console.WriteLine("Nome: ");
+                    nome = Console.ReadLine();
+
+                    Console.WriteLine("Telefone: ");
+                    telefone = Console.ReadLine();
+
+                    Console.WriteLine("Email: ");
+                    email = Console.ReadLine();
+
+                    list.Add(new Contato(nome, telefone, email));
+                }
+                else if (menu == 2)
+                {
+                    string email;
+                    Console.WriteLine("Digite um email: ");
+                    email = Console.ReadLine();
+                    Console.WriteLine("Procurando contato.");
+
+                    if (list.DeleteNode(email))
+                    {
+                        Console.WriteLine("Contato encontrado e deletado.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("O contato não foi deletado.");
+                    }
+                }
+                else if (menu == 3)
+                {
+                    string email;
+                    Console.WriteLine("Digite o email: ");
+                    email = Console.ReadLine();
+                    Console.WriteLine("Procurando contato para atualizar.");
+
+                    Contato contato = list.Find(email);
+
+                    if (contato != null)
+                    {
+                        Console.WriteLine("Editando usuário.");
+                        Console.WriteLine(contato);
+
+                        Console.WriteLine("Nome: ");
+                        contato.nome = Console.ReadLine();
+
+                        Console.WriteLine("Telefone: ");
+                        contato.telefone = Console.ReadLine();
+
+                        Console.WriteLine("Email: ");
+                        contato.email = Console.ReadLine();
+
+                        Console.WriteLine("Contato Atualizado.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("O arquivo não foi carregado");
+                    }
+                }
+                else if (menu == 4)
+                {
+                    Console.WriteLine("Recuperando do arquivo...");
+
+                    if (list.LoadFile("arquivo.txt"))
+                    {
+                        Console.WriteLine("O arquivo foi carregado.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("O arquivo não foi carregado.");
+                    }
+                }
+                else if (menu == 5)
+                {
+                    int sortOption;
+
+                    Console.WriteLine("Escolha a opção de ordenação.");
+                    Console.WriteLine("1 - Nome");
+                    Console.WriteLine("2 - Email");
+                    Console.WriteLine("Opção: ");
+
+                    sortOption = Convert.ToInt32(Console.ReadLine());
+                    if (sortOption == 1)
+                    {
+                        list.BubbleSort("nome");
+                    }
+                    else if (sortOption == 2)
+                    {
+                        list.BubbleSort("email");
+                    }
+                    Console.Clear();
+                }
+                else if (menu == 6)
+                {
+                    Console.WriteLine("Listando Contatos:");
+                    list.Print();
+                }
+                else if (menu == 7)
+                {
+                    Console.WriteLine("Salvando o arquivo...");
+
+                    if (list.PrintFile("arquivo.txt"))
+                    {
+                        Console.WriteLine("Arquivo salvo.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("O arquivo não foi salvo.");
+                    }
+
+                }
+                else if (menu == 8)
+                {
+                    if (list.head == null)
+                    {
+                        Console.WriteLine("Lista vazia.");
+                        return;
+                    }
+
+                    Node temp = list.head;
+
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Navegação:");
+                        Console.WriteLine("Seta esquerda: contato anterior");
+                        Console.WriteLine("Seta direita: próximo contato");
+                        Console.WriteLine("Precione qualquer tecla para sair.");
+                        Console.WriteLine("Contato Atual:");
+                        Console.WriteLine(temp.data);
+
+                        string key = Console.ReadKey().Key.ToString();
+
+                        if (key == "LeftArrow")
+                        {
+                            temp = temp.prev;
+                        }
+                        else if (key == "RightArrow")
+                        {
+                            temp = temp.next;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
