cdd5a6b8845206732e67c4bc5223926116cef014
Lanes Rocha <lanes.rocha@admin.uvv>
2020-11-25 19:54:42 -0300

    Atualização da class Node.cs criação do Nó...

--- aula08 - ListaOrdenada/Program.cs
+++ aula08 - ListaOrdenada/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace aula08___ListaOrdenada
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            
-        }
+        
     }
 }
