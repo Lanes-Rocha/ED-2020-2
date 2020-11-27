0897e0182952623319bfe82e9eaa973e26fda426
Lanes Rocha <lanes.rocha@hotmail.com>
2020-11-27 01:42:18 -0300

    Criação da class Node.cs

--- aula10 - Pilha/Node.cs
+++ aula10 - Pilha/Node.cs
@@ -1,8 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
 public class Node
 {
-    
+    public int data;
+    public Node next; //criando um objeto next
+
+    public Node() // construtor 
+    {
+        data = -1;
+        next = null;
+    }
+    public Node(int valor)
+    {
+        data = valor;
+        next = null;
+    }
 }
