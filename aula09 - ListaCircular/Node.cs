99eba1458ab93cf76d9d872ffc6fc7f3afe19368
Lanes Rocha <lanes.rocha@admin.uvv>
2020-11-26 21:18:52 -0300

    Criação das classes ListaCircular.cs e Node.cs

---
+++ aula09 - ListaCircular/Node.cs
@@ -0,0 +1,17 @@
+
+class Node
+{
+     public int data;
+    public Node next; //criando um objeto next
+
+    public Node()// construtor 
+    {
+        data = -1;
+        next = null;
+    }
+    public Node(int valor)
+    {
+        data = valor;
+        next = null;
+    }
+}
